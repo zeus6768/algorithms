01a5d118c779c66e8e4b4c4d431c4a89efeeed41
Zeus6768 <akaps6768@gmail.com>
2023-11-28 10:45:03 +0900

    2023 11 28 solve 1

--- src/main/java/ac/solved/class3/Bj1389.java
+++ src/main/java/ac/solved/class3/Bj1389.java
@@ -1,48 +1,98 @@
 package ac.solved.class3;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Bj1389 {
 
-    static int n, m;
-    static List<Integer>[] graph;
-    static int[] scores;
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer tokenizer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[n+1];
-        scores = new int[n+1];
-        for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
+    int n, m;
+    List<Set<Integer>> graph;
+
+    public static void main(String[] args) {
+        new Bj1389().solve();
+    }
+
+    void solve() {
+        init();
+        System.out.println(getAnswer());
+    }
+
+    void init() {
+        input();
+        n = getIntToken();
+        m = getIntToken();
+        initGraph();
+    }
+
+    int getAnswer() {
+        int answer = 0;
+        int minKevinBacon = Integer.MAX_VALUE;
+        for (int user = 1; user <= n; user++) {
+            int kevinBacon = getKevinBaconFrom(user);
+            if (minKevinBacon > kevinBacon) {
+                minKevinBacon = kevinBacon;
+                answer = user;
+            }
         }
-        m = Integer.parseInt(st.nextToken());
+        return answer;
+    }
+
+    void initGraph() {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new HashSet<>());
+        }
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+            input();
+            int friendA = getIntToken();
+            int friendB = getIntToken();
+            addRelationship(friendA, friendB);
         }
     }
 
-    static void bfs(int start) {
-        boolean[][] visited = new boolean[n+1][n+1];
-        visited[start][start] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+    void addRelationship(int friend1, int friend2) {
+        graph.get(friend1).add(friend2);
+        graph.get(friend2).add(friend1);
+    }
+
+    int getKevinBaconFrom(int start) {
+        int kevinBacon = 0;
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.offer(List.of(start, 0));
+        boolean[] visited = new boolean[n+1];
+        visited[start] = true;
         while (!queue.isEmpty()) {
-            int now = queue.poll();
-            for (int next : graph[now]) {
-                if (!visited[now][next]) {
-                    visited[now][next] = true;
-                    queue.offer(next);
+            List<Integer> now = queue.poll();
+            int user = now.get(0);
+            int step = now.get(1);
+            kevinBacon += step;
+            for (int nextUser : graph.get(user)) {
+                if (!visited[nextUser]) {
+                    visited[nextUser] = true;
+                    queue.offer(List.of(nextUser, step+1));
                 }
             }
         }
+        return kevinBacon;
+    }
+
+    void input() {
+        try {
+            tokenizer = new StringTokenizer(bufferedReader.readLine());
+        } catch (Exception ignored) {}
+    }
+
+    int getIntToken() {
+        return Integer.parseInt(tokenizer.nextToken());
     }
 }
