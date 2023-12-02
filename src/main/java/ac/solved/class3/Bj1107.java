ce6c6b37ac38b100c5fd02890f1cb17acb3c61a9
Zeus6768 <akaps6768@gmail.com>
2023-12-02 23:12:37 +0900

    2023 12 02 update

---
+++ src/main/java/ac/solved/class3/Bj1107.java
@@ -0,0 +1,58 @@
+package ac.solved.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj1107 {
+
+    int n, m;
+    boolean[] isBroken = new boolean[10];
+
+    int answer;
+
+    public static void main(String[] args) {
+        new Bj1107().solve();
+    }
+
+    void solve() {
+        init();
+        getMinPushCount();
+        System.out.println(answer);
+    }
+
+    void init() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            m = Integer.parseInt(br.readLine());
+            if (m != 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int i = 0; i < m; i++) {
+                    int button = Integer.parseInt(st.nextToken());
+                    isBroken[button] = true;
+                }
+            }
+            answer = Math.abs(n - 100);
+        } catch (Exception ignored) {}
+    }
+
+    void getMinPushCount() {
+        for (int i = 0; i <= 999_999; i++) {
+            String target = String.valueOf(i);
+            int length = target.length();
+            boolean isButtonOk = true;
+            for (int j = 0; j < length; j++) {
+                int button = target.charAt(j) - '0';
+                if (isBroken[button]) {
+                    isButtonOk = false;
+                    break;
+                }
+            }
+            if (isButtonOk) {
+                int pushCount = length + Math.abs(n - i);
+                answer = Math.min(answer, pushCount);
+            }
+        }
+    }
+}
---
+++ src/main/java/ac/solved/class3/Bj1463.java
@@ -0,0 +1,44 @@
+package ac.solved.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj1463 {
+
+    int n;
+    int[] dp;
+
+    public static void main(String[] args) {
+        new Bj1463().solve();
+    }
+
+    void solve() {
+        init();
+        getOperationCount();
+        System.out.println(dp[1]);
+    }
+
+    void init() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            dp = new int[n+1];
+            dp[n] = 0;
+            for (int i = 0; i < n; i++) {
+                dp[i] = Integer.MAX_VALUE;
+            }
+        } catch (Exception ignored) {}
+    }
+
+    void getOperationCount() {
+        for (int i = n; i > 0; i--) {
+            if (i % 3 == 0) {
+                dp[i/3] = Math.min(dp[i/3], dp[i] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i/2] = Math.min(dp[i/2], dp[i] + 1);
+            }
+            dp[i-1] = Math.min(dp[i-1], dp[i] + 1);
+        }
+    }
+}
