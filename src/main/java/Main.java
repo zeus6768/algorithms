48d33631d4fc6a8e385ad116d085c02c21b45441
Zeus6768 <akaps6768@gmail.com>
2024-02-09 20:21:33 +0900

    2024 02 09 : 1 solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
-import study.MinStack;
-
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import study.MinStack;
 
 public class Main {
 
@@ -15,7 +19,22 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        Set<List<Integer>> hashSet = new HashSet<>();
+        Set<List<Integer>> linkedHashSet = new LinkedHashSet<>();
+        test(hashSet);
+        test(linkedHashSet);
+    }
 
+    static void test(Set<List<Integer>> set) {
+        List<Integer> list1 = List.of(1, 2);
+        List<Integer> list2 = List.of(1, 2);
+        List<Integer> list3 = List.of(1, 3);
+        set.add(list1);
+        set.add(list2);
+        set.add(list3);
+        for (List<Integer> numbers : set) {
+            System.out.println(numbers);
+        }
     }
 }
 
